7c0a17b1f4d3624994f911b73d7a70082de8a1c4
Samuel Reyes Alvarez <>
2019-01-10 19:56:32 +0100

    Tests parametrizados realizados

--- test/barraprogreso/BarraProgresoParameterizedTest.java
+++ test/barraprogreso/BarraProgresoParameterizedTest.java
@@ -1,11 +1,12 @@
 package barraprogreso;
 
 import java.util.Arrays;
-import javax.swing.JFrame;
 import org.junit.After;
 import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
@@ -18,19 +19,23 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(Parameterized.class)
 public class BarraProgresoParameterizedTest {
 
-    private int min, max, valor;
-    private JFrame ventanaPadre, ventanaSiguiente;
+    private BarraProgreso instance;
+    private int numero, resultado;
 
     @Parameters
     public static Iterable data() {
         return Arrays.asList(new Object[][]{
-            {3, 3}, {5, 5}, {5, 4}
+            {0, 0}, {100, 100}, {0, 100} // se esperan 2 pruebas exitosas y 1 fallida
         });
+    }
+
+    public BarraProgresoParameterizedTest(int numero, int resultado) {
+        this.numero = numero;
+        this.resultado = resultado;
     }
 
     @BeforeClass
     public static void setUpClass() {
-
     }
 
     @AfterClass
@@ -39,9 +44,41 @@ public class BarraProgresoParameterizedTest {
 
     @Before
     public void setUp() {
+        instance = new BarraProgreso();
     }
 
     @After
     public void tearDown() {
+        instance = null;
+    }
+
+    /**
+     * Test of setMinimo method, of class BarraProgreso.
+     */
+    @Test
+    public void testSetMinimo() {
+        System.out.println("setMinimo");
+        instance.setMinimo(numero);
+        assertEquals(resultado, instance.getMinimo());
+    }
+
+    /**
+     * Test of setMaximo method, of class BarraProgreso.
+     */
+    @Test
+    public void testSetMaximo() {
+        System.out.println("setMaximo");
+        instance.setMaximo(numero);
+        assertEquals(resultado, instance.getMaximo());
+    }
+
+    /**
+     * Test of setProgreso method, of class BarraProgreso.
+     */
+    @Test
+    public void testSetProgreso() {
+        System.out.println("setProgreso");
+        instance.setProgreso(numero);
+        assertEquals(resultado, instance.getProgreso());
     }
 }
