720888c71d9dcefd9d91707c695de0531c58421d
Samuel Reyes Alvarez <Usuario@DESKTOP-F6NI6OO>
2019-01-14 00:03:31 +0100

    Actualizado a version 3.1
    Incluida la Test Suite del Bean
    Corregidos e incluidos comentarios para la generacion del Javadoc
    
    Pendiente generar documento javadoc (se producen 1 error y 26 warnings)
    <BUILD FAILED>

--- src/barraprogreso/BarraProgreso.java
+++ src/barraprogreso/BarraProgreso.java
@@ -8,7 +8,7 @@ import javax.swing.border.Border;
 
 /**
  *
- * @author Samuel Reyes
+ * @author Samuel Reyes Álvarez
  *
  * Bean personalizado de JProgressBar que incluye nuevos constructores que
  * reciben como parametros la ventana que posee la barra de progreso y la
@@ -16,23 +16,56 @@ import javax.swing.border.Border;
  * metodo que se ancarga de cerrar la ventana que contiene la barra y abre la
  * siguiente ventana cuando el progreso de la barra alcanza el 100%.
  *
+ * @version 3.1
+ * @date 14/01/2019
+ *
  */
 public class BarraProgreso extends JProgressBar implements Serializable {
 
     // atributos basicos
+    /**
+     * Valor minimo de la barra
+     */
     private int minimo;
+    /**
+     * Valor maximo de la barra
+     */
     private int maximo;
+    /**
+     * Valor de progreso de la barra
+     */
     private int progreso;
+    /**
+     * Texto que aparece sobre la barra
+     */
     private String texto;
+    /**
+     * Borde de la barra
+     */
     private Border borde;
 
     // opciones de ventanas disponibles
+    /**
+     * Ventana que contiene la barra es tipo JFrame
+     */
     private JFrame framePadre = null;
+    /**
+     * Ventana que contiene la barra es tipo JDialog
+     */
     private JDialog dialogPadre = null;
+    /**
+     * Ventana que la barra abrira es tipo JFrame
+     */
     private JFrame frameNuevo = null;
+    /**
+     * Ventana que la barra abrira es tipo JDialog
+     */
     private JDialog dialogNuevo = null;
 
-    // este atributo se usa para controlar la combinacion de ventanas elegida
+    /**
+     * Combinacion de ventanas elegida {framePadre, dialogPadre, frameNuevo,
+     * dialogNuevo}
+     */
     private int[] codigo = {0, 0, 0, 0};
 
     /**
@@ -43,7 +76,7 @@ public class BarraProgreso extends JProgressBar implements Serializable {
     }
 
     /**
-     * Constructores con las diferentes combinaciones de tipos de ventanas
+     * Constructores con framePadre + frameNuevo
      *
      * @param ventanaPadre
      * @param ventanaNueva
@@ -54,18 +87,36 @@ public class BarraProgreso extends JProgressBar implements Serializable {
         ventanaNueva.setVisible(false);
     }
 
+    /**
+     * Constructor con framePadre + dialogNuevo
+     *
+     * @param ventanaPadre
+     * @param ventanaNueva
+     */
     public BarraProgreso(JFrame ventanaPadre, JDialog ventanaNueva) {
         setVentanaPadre(ventanaPadre);
         setVentanaNueva(ventanaNueva);
         ventanaNueva.setVisible(false);
     }
 
+    /**
+     * Constructor con dialogPadre + frameNuevo
+     *
+     * @param ventanaPadre
+     * @param ventanaNueva
+     */
     public BarraProgreso(JDialog ventanaPadre, JFrame ventanaNueva) {
         setVentanaPadre(ventanaPadre);
         setVentanaNueva(ventanaNueva);
         ventanaNueva.setVisible(false);
     }
 
+    /**
+     * Constructor con dialogPadre + dialogNuevo
+     *
+     * @param ventanaPadre
+     * @param ventanaNueva
+     */
     public BarraProgreso(JDialog ventanaPadre, JDialog ventanaNueva) {
         setVentanaPadre(ventanaPadre);
         setVentanaNueva(ventanaNueva);
@@ -73,8 +124,7 @@ public class BarraProgreso extends JProgressBar implements Serializable {
     }
 
     /**
-     * Metodos que definen el tipo de ventana que contiene a la barra de
-     * progreso
+     * Establece que la ventana que contiene a la barra es un JFrame
      *
      * @param ventanaPadre
      */
@@ -87,6 +137,11 @@ public class BarraProgreso extends JProgressBar implements Serializable {
         }
     }
 
+    /**
+     * Establece que la ventana que contiene a la barra es un JDialog
+     *
+     * @param ventanaPadre
+     */
     public void setVentanaPadre(JDialog ventanaPadre) {
         if (ventanaPadre != null) {
             dialogPadre = ventanaPadre;
@@ -97,8 +152,7 @@ public class BarraProgreso extends JProgressBar implements Serializable {
     }
 
     /**
-     * Metodos que definen el tipo de ventana que se abrira al completarse la
-     * carga de la barra de progreso
+     * Establece que la ventana que la barra abrira es un JFrame
      *
      * @param ventanaNueva
      */
@@ -111,6 +165,11 @@ public class BarraProgreso extends JProgressBar implements Serializable {
         }
     }
 
+    /**
+     * Establece que la ventana que la barra abrira es un JDialog
+     *
+     * @param ventanaNueva
+     */
     public void setVentanaNueva(JDialog ventanaNueva) {
         if (ventanaNueva != null) {
             dialogNuevo = ventanaNueva;
@@ -121,8 +180,7 @@ public class BarraProgreso extends JProgressBar implements Serializable {
     }
 
     /**
-     * Metodo sobrecargado que establece los valores minimo y maximo de la barra
-     * y otro que incluye el progreso actual ademas de los anteriores
+     * Se encarga de establecer los valores minimo y maximo de la barra
      *
      * @param min
      * @param max
@@ -137,7 +195,16 @@ public class BarraProgreso extends JProgressBar implements Serializable {
         }
     }
 
-    public void setValores(int min, int max, int progresoActual) {
+    /**
+     * Se encarga de establecer los valores minimo, maximo y de progreso de la
+     * barra
+     *
+     * @param min
+     * @param max
+     * @param progresoActual
+     * @throws IllegalArgumentException
+     */
+    public void setValores(int min, int max, int progresoActual) throws IllegalArgumentException {
         setValores(min, max);
         if (min <= progresoActual && progresoActual <= max) {
             setProgreso(progresoActual);
@@ -147,9 +214,9 @@ public class BarraProgreso extends JProgressBar implements Serializable {
     }
 
     /**
-     * Metodo que establece el estilo del borde y el texto que aparecera dentro
-     * de la barra de progreso y los activa para que sean visibles. Si alguno de
-     * ellos se define como null, se desactivara
+     * Establece el estilo del borde y el texto que aparecera dentro de la barra
+     * de progreso y los activa para que sean visibles. Si alguno de ellos se
+     * define como null, se desactivara
      *
      * @param border
      * @param texto
@@ -171,92 +238,156 @@ public class BarraProgreso extends JProgressBar implements Serializable {
     }
 
     /**
-     * Metodos Getters y Setters basicos
+     * Devuelve el valor definido como minimo en la barra
      *
-     * @return valores basicos de la barra de progreso
+     * @return minimo
      */
     public int getMinimo() {
         minimo = getMinimum();
         return minimo;
     }
 
+    /**
+     * Establece el valor minimo de la barra
+     *
+     * @param minimo
+     */
     public void setMinimo(int minimo) {
         this.minimo = minimo;
         this.setMinimum(minimo);
     }
 
+    /**
+     * Devuelve el valor establecido como maximo en la barra
+     *
+     * @return maximo
+     */
     public int getMaximo() {
         maximo = getMaximum();
         return maximo;
     }
 
+    /**
+     * Establece el valor maximo de la barra
+     *
+     * @param maximo
+     */
     public void setMaximo(int maximo) {
         this.maximo = maximo;
         this.setMaximum(maximo);
     }
 
+    /**
+     * Devuelve el progreso actual de la barra
+     *
+     * @return progreso
+     */
     public int getProgreso() {
         progreso = getValue();
         return progreso;
     }
 
+    /**
+     * Establece el progreso actual de la barra
+     *
+     * @param progreso
+     */
     public void setProgreso(int progreso) {
         this.progreso = progreso;
         this.setValue(progreso);
     }
 
+    /**
+     * Devuelve el texto establecido en la barra
+     *
+     * @return texto
+     */
     public String getTexto() {
         texto = getString();
         return texto;
     }
 
+    /**
+     * Establece el texto que se mostrara encima de la barra
+     *
+     * @param texto
+     */
     public void setTexto(String texto) {
         this.texto = texto;
         this.setString(texto);
     }
 
+    /**
+     * Devuelve el borde definido en la barra
+     *
+     * @return borde
+     */
     public Border getBorde() {
         borde = getBorder();
         return borde;
     }
 
+    /**
+     * Establece el borde de la barra
+     *
+     * @param borde
+     */
     public void setBorde(Border borde) {
         this.borde = borde;
         this.setBorder(borde);
     }
 
     /**
-     * Metodos Getters de las ventanas elegidas
+     * Devuelve el JFrame que contiene a la barra
      *
-     * @return
+     * @return framePadre
      */
     public JFrame getFramePadre() {
         return framePadre;
     }
 
+    /**
+     * Devuelve el JDialog que contiene a la barra
+     *
+     * @return dialogPadre
+     */
     public JDialog getDialogPadre() {
         return dialogPadre;
     }
 
+    /**
+     * Devuelve el JFrame que la barra abre
+     *
+     * @return frameNuevo
+     */
     public JFrame getFrameNuevo() {
         return frameNuevo;
     }
 
+    /**
+     * Devuelve el JDialog que la barra abre
+     *
+     * @return dialogNuevo
+     */
     public JDialog getDialogNuevo() {
         return dialogNuevo;
     }
 
+    /**
+     * Devuelve un array de int con los valores minimo, maximo y progreso actual
+     *
+     * @return valores
+     */
     public int[] getValores() {
         int[] valores = {getMinimo(), getMaximo(), getProgreso()};
         return valores;
     }
 
     /**
-     * Metodo que realiza la transicion entre ventanas si el progreso esta
-     * completo
+     * Realiza la transicion entre ventanas si el progreso esta completo al 100%
      */
     public void cambioDeVentana() {
-        if (this.getPercentComplete() == 1.0) {
+        if (this.getPercentComplete() > 0.9) {
             int codigoFacil = codigo[0] * 8 + codigo[1] * 4 + codigo[2] * 2 + codigo[3] * 1;
 
             switch (codigoFacil) {
